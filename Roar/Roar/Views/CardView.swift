f41fbf7cfc11e174d12a2f87375e9e5530d51fdd
mindelicious <[mailprogramisty@gmail.com]>
2019-03-08 22:15:06 +0100

    The skleleton project with swipping card

---
+++ Roar/Roar/Views/CardView.swift
@@ -0,0 +1,42 @@
+//
+//  CardView.swift
+//  Roar
+//
+//  Created by Matt on 07/02/2019.
+//  Copyright © 2019 mindelicious. All rights reserved.
+//
+
+import UIKit
+
+extension ViewController {
+    
+    
+    func setupDummyCard() {
+
+        //        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear"))
+        let newImage = UIImage(named: "bear")
+        animalView.image = newImage
+        animalLabel.text = "bear"
+        
+        
+        //Gesture
+        //origin setup
+        //        originCardPoint = cardView.frame.origin
+        
+    }
+    // MARK: - Gradient On a textView
+    func gradientTextView() {
+        
+        let gradientLayer = CAGradientLayer()
+        textView.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+        gradientLayer.frame = textView.bounds
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0, 0.1, 0.5, 1]
+        textView.layer.mask = gradientLayer
+        
+    }
+    
+    
+    
+    
+}
