cb8f2ef7513e13f3f3d1d8d36dc6882b1f734a9a
mindelicious <[mailprogramisty@gmail.com]>
2019-04-20 08:14:23 +0200

    AnimalBnak added

---
+++ Roar/Roar/Model/AnimalsStock.swift
@@ -0,0 +1,29 @@
+//
+//  AnimalsStock.swift
+//  Roar
+//
+//  Created by Matt on 20/04/2019.
+//  Copyright © 2019 mindelicious. All rights reserved.
+//
+
+import Foundation
+
+class AnimalsStock {
+    
+    var list = [Animal]()
+    init() {
+        
+        let item =  Animal(name: "Bear", soundUrl: "Bear", imageName: "Bear")
+        
+        list.append(item)
+        
+        list.append(Animal(name: "Cat", soundUrl: "Cat", imageName: "Cat"))
+        list.append(Animal(name: "Chicken", soundUrl: "Chicken", imageName: "Chicken"))
+        list.append(Animal(name: "Cow", soundUrl: "Cow", imageName: "Cow"))
+        list.append(Animal(name: "Dog", soundUrl: "Dog", imageName: "Dog"))
+        list.append(Animal(name: "Donkey", soundUrl: "Donkey", imageName: "Donkey"))
+        list.append(Animal(name: "Duck", soundUrl: "Duck", imageName: "Duck"))
+        list.append(Animal(name: "Elephant", soundUrl: "Elephant", imageName: "Elephant"))
+        
+    }
+}
